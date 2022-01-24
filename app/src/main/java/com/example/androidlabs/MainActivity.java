3567cec8d4e175dd07f6e7c182cd87ea06734578
Naz <lespaulchichpea@gmail.com>
2022-01-24 18:08:05 -0500

    Lab 1

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CheckBox checkBoxVisibility;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_grid);
+
+        CheckBox cb = findViewById(R.id.check);
+
+        Button button = findViewById(R.id.button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,getResources().getString(R.string.toast_message), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        cb.setOnCheckedChangeListener( (compoundButton, b)  -> {
+
+            Snackbar snackbar = Snackbar.make(cb, this.getResources().getString(R.string.onCheck) + " " + b, Snackbar.LENGTH_LONG);
+                    snackbar.setAction("Undo", click-> compoundButton.setChecked( !b ))
+                            .show();
+        });
+
+
     }
-}
+
+    }
+
