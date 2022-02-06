845ea3cd0d157ebfbab220ad96a3939db7069218
Naz <lespaulchichpea@gmail.com>
2022-02-06 15:06:16 -0500

    Lab 4 done

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,72 +1,165 @@
 package com.example.androidlabs;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.graphics.Color;
 import android.os.Bundle;
-import android.text.Editable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText EDid;
-    private Button MyBut;
+    private ArrayList<TodoItem> elements;
+    private MyListAdapter myAdapter;
+    private Button button;
+    private Switch aswitch;
+    private EditText eText;
 
-    public static final String SHARED_PREFS = "sharedPrefs";
-    public static final String TEXT = "text";
-
-    private String Text;
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        saveData();
-        Toast.makeText(this, "On Pause", Toast.LENGTH_SHORT).show();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        // setContentView loads objects onto the screen.
+        // Before this function, the screen is empty.
         setContentView(R.layout.activity_main);
-        EDid = findViewById(R.id.EDid);
-        MyBut = findViewById(R.id.MyBut);
+        MainActivity.this.elements = new ArrayList<>();
+        ListView myList = findViewById(R.id.lView);
+        myAdapter = new MyListAdapter(elements);
+        myList.setAdapter(myAdapter);
+        eText = findViewById(R.id.Etext);
+        aswitch = findViewById(R.id.urgent);
+        button = findViewById(R.id.addButton);
+        initAddButton();
 
-        MyBut.setOnClickListener(new View.OnClickListener(){
+
+        myList.setOnItemLongClickListener((p, b, pos, id) -> {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setTitle("Do you want to delete this?")
+
+
+                    .setMessage("Selected row is: " + pos)
+
+
+                    .setPositiveButton("Yes", (click, arg) -> {
+                        elements.remove(pos);
+                        myAdapter.notifyDataSetChanged();
+                    })
+
+                    .setNegativeButton("No", (click, arg) -> {
+                    })
+
+
+                    .setView(getLayoutInflater().inflate(R.layout.activity_todo, null))
+
+                    .create().show();
+            return true;
+        });
+    }
+
+    private void initAddButton() {
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                String text = "Welcome " + EDid.getText().toString() + "!";
+            public void onClick(View view) {
 
-                Intent intent = new Intent(MainActivity.this,NameActivity.class);
-                intent.putExtra("Text", text);
-                startActivityForResult(intent, 1);
+                elements.add(new TodoItem(eText.getText().toString(), aswitch.isChecked()));
+                myAdapter.notifyDataSetChanged();
+                eText.setText("");
+                aswitch.setChecked(false);
             }
         });
 
-        loadData();
-        updateViews();
+
     }
 
-    public void saveData(){
-        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
+    private class MyListAdapter extends BaseAdapter {
 
-        editor.putString(TEXT, EDid.getText().toString());
+        private ArrayList<TodoItem> jobby;
 
-        editor.apply();
+        public MyListAdapter(ArrayList<TodoItem> jobby) {
+            this.jobby = jobby;
+
+        }
+
+        public int getCount() {
+            return jobby.size();
+        }
+
+        public Object getItem(int position) {
+            return jobby.get(position);
+
+        }
+
+        public long getItemId(int position) {
+            return (long) position;
+        }
+
+        public View getView(int position, View old, ViewGroup parent) {
+            View newView = old;
+            LayoutInflater inflater = getLayoutInflater();
+
+
+            if (newView == null) {
+                newView = inflater.inflate(R.layout.activity_todo, parent, false);
+
+            }
+
+            TextView tView = newView.findViewById(R.id.textGoesHere);
+            TodoItem jon = (TodoItem) getItem(position);
+            tView.setText((jon).getText());
+            //tView.setText(((TodoItem)getItem(position)).getText());
+
+            //set what the text should be for this row:
+
+            if (elements.get(position).isUrgent()) {
+                newView.setBackgroundColor(Color.RED);
+                tView.setTextColor(Color.WHITE);
+            } else {
+                newView.setBackgroundColor(Color.WHITE);
+                tView.setTextColor(Color.BLACK);
+            }
+            //return it to be put in the table
+            return newView;
+        }
     }
 
-    public void loadData(){
+    public class TodoItem {
 
-        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
-        Text = sharedPreferences.getString(TEXT, "");
-    }
+        private String text;
+        private boolean isUrgent;
 
-    public void updateViews(){
-        EDid.setText(Text);
+        public TodoItem(String text, boolean isUrgent) {
+            this.text = text;
+            this.isUrgent = isUrgent;
 
+
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public boolean isUrgent() {
+            return isUrgent;
+        }
     }
 }
